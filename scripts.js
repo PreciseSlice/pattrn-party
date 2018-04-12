80132e1b7d685b43e6f8d73242de34732bb909a1
Spencer Herms <slherms@gmail.com>
2018-04-11 21:07:09 -0600

    add click events for codepen links

--- scripts.js
+++ scripts.js
@@ -5,7 +5,6 @@ $('.tab-list').on('click', '.tab', event => {
     .parent()
     .removeClass('selected-div');
   $('.tab-content').removeClass('show');
-
   $(event.target.parentElement).addClass('selected-div');
   $(event.target).addClass('active');
   $($(event.target).attr('href')).addClass('show');
@@ -17,5 +16,17 @@ $('.menu-btn').on('click', event => {
   $('.nav-a').toggleClass('mobile-nav');
 });
 
-// link to accordian tabs
-// https://codepen.io/PreciseSlice/pen/oqENWV
+$('.accordion-code').on('click', event => {
+  event.preventDefault();
+  window.open('https://codepen.io/PreciseSlice/pen/oqENWV', '_blank');
+});
+
+$('.card-code').on('click', event => {
+  event.preventDefault();
+  window.open('https://codepen.io/PreciseSlice/pen/ivTWXA', '_blank');
+});
+
+$('.nav-code').on('click', event => {
+  event.preventDefault();
+  window.open('https://codepen.io/PreciseSlice/pen/pwPFJW', '_blank');
+});
